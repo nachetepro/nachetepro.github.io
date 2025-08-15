71b1b624a7b507acdd59a4a3da7b56922c2f3875
nachetepro <n4sabo@gmail.com>
2025-08-15 14:58:37 -0500

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "images.unsplash.com",
+      "loveme.es",
+      "www.dynamicgc.es",
+      "www.bbva.com",
+    ],
+  },
 };
 
 export default nextConfig;
