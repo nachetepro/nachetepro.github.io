b59e734b179a896e075464820b3493ca3bd17751
nachetepro <n4sabo@gmail.com>
2025-08-15 04:10:06 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -87,7 +87,7 @@ function Header() {
         <motion.img 
           src="/logo.png" 
           alt="Logo" 
-          className="h-22 w-22 object-contain rounded-xl shadow-md"
+          className="h-19 w-19 object-contain rounded-xl shadow-md"
           animate={{ y: [0, -6, 0], rotate: [0, 2, -2, 0] }}
           transition={{ repeat: Infinity, duration: 4, ease: "easeInOut" }}
         />
